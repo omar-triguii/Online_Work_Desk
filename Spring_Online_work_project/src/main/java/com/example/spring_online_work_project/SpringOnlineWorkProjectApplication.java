fcef6c57113c6f3acf3885aab61eba696070c700
omar trigui <omar.trigui@supcom.tn>
2022-02-18 13:06:21 +0100

    first commit

---
+++ Spring_Online_work_project/src/main/java/com/example/spring_online_work_project/SpringOnlineWorkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_online_work_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineWorkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOnlineWorkProjectApplication.class, args);
+    }
+
+}
---
+++ Spring_Online_work_project/src/test/java/com/example/spring_online_work_project/SpringOnlineWorkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_online_work_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnlineWorkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
