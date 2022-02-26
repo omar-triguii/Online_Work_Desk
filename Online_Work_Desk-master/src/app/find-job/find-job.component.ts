d61fa408ec78ae07079120a1e46a9b3dbfe81957
Mouadh Tira <mouadhtira@gmail.com>
2022-02-26 01:39:28 +0100

    add some comment and add job card group

--- Online_Work_Desk-master/src/app/find-job/find-job.component.ts
+++ Online_Work_Desk-master/src/app/find-job/find-job.component.ts
@@ -3,14 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-find-job',
   templateUrl: './find-job.component.html',
-  styleUrls: ['./find-job.component.css']
+  styleUrls: ['./find-job.component.css'],
 })
 export class FindJobComponent implements OnInit {
-  test:number[]=[1,2,3,4,5,6,7,8,9,10];
-  constructor() { }
+  jobs: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  locationList:string[]=['Ariana','Béja','Ben Arous','Bizerte','Gabès','Gafsa','Jendouba','Kairouan','Kasserine','Kébili','Kef','Mahdia','Manouba','Médenine','Monastir','Nabeul','Sfax','Sidi Bouzid','Siliana','Sousse','Tataouine','Tozeur','Tunis','Zaghouan']
-  jobList:string[]=["banay  ","engineer  ","dahan  "];
+  ngOnInit(): void {}
+  locationList: string[] = [
+    'Ariana',
+    'Béja',
+    'Ben Arous',
+    'Bizerte',
+    'Gabès',
+    'Gafsa',
+    'Jendouba',
+    'Kairouan',
+    'Kasserine',
+    'Kébili',
+    'Kef',
+    'Mahdia',
+    'Manouba',
+    'Médenine',
+    'Monastir',
+    'Nabeul',
+    'Sfax',
+    'Sidi Bouzid',
+    'Siliana',
+    'Sousse',
+    'Tataouine',
+    'Tozeur',
+    'Tunis',
+    'Zaghouan',
+  ];
+  jobList: string[] = ['banay  ', 'engineer  ', 'dahan  '];
 }
