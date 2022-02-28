48ddff15ecb7af67ad6c50f873af3377827a990a
Firas Yahyaoui <firas.yahyaoui@supcom.tn>
2022-02-28 13:07:10 +0100

    integrate home page

--- Online_Work_Desk-master/src/app/auth-service.service.ts
+++ Online_Work_Desk-master/src/app/auth-service.service.ts
@@ -8,52 +8,52 @@ const AUTH_API = 'http://localhost:8087/user';
   providedIn: 'root'
 })
 export class AuthServiceService {
-  helper= new JwtHelperService
+  helper = new JwtHelperService
   constructor(private http: HttpClient) { }
 
-  register(body:any): Observable<any> {
-    return this.http.post('http://localhost:8087/user/register',body);
+  register(body: any): Observable<any> {
+    return this.http.post('http://localhost:8087/user/register', body);
   }
 
-  login(body:any){
-    return this.http.post('http://localhost:8087/user/auth',body)
+  login(body: any) {
+    return this.http.post('http://localhost:8087/user/auth', body)
   }
-  getuserbyemail(email:string){
+  getuserbyemail(email: string) {
     return this.http.get(`http://localhost:8087/user/omar/${email}`)
   }
-  getusernamebyemail(email:string){
-    return this.http.get(`http://localhost:8087/user/trigui/${email}`,{responseType: 'text'})
+  getusernamebyemail(email: string) {
+    return this.http.get(`http://localhost:8087/user/trigui/${email}`, { responseType: 'text' })
   }
- /*
-  getadmin(email:String){
+  /*
+   getadmin(email:String){
 
-    return this.http.get(`http://localhost:8081/getadmin/${email}`)
-  }*/
+     return this.http.get(`http://localhost:8081/getadmin/${email}`)
+   }*/
 
-  saveDataProfil(token:any){
+  saveDataProfil(token: any) {
 
     //  let decodeToken= this.helper.decodeToken(token)
 
-     localStorage.setItem('token',token)
-    let decodeToken=this.helper.decodeToken(token)
+    localStorage.setItem('token', token)
+    let decodeToken = this.helper.decodeToken(token)
 
     console.log(decodeToken.sub)
 
+  }
+  getEmail() {
+    let token: any = localStorage.getItem('token')
+    let decodeToken = this.helper.decodeToken(token)
+    return decodeToken.sub
+  }
+  loggedIn() {
+    let token: any = localStorage.getItem('token')
+    let decodeToken = this.helper.decodeToken(token)
+    if (this.helper.isTokenExpired(token)) {
+      return false
     }
-    getEmail(){
-     let token:any= localStorage.getItem('token')
-    let decodeToken=this.helper.decodeToken(token)
-      return decodeToken.sub
-    }
-    loggedIn(){
-      let token:any=localStorage.getItem('token')
-      let decodeToken=this.helper.decodeToken(token)
-      if(this.helper.isTokenExpired(token)){
-        return false
-      }
-      return true
-    }
-    logout() :Observable<any>{
-      return this.http.get('http://localhost:8087/user/logout',{responseType: 'text'})
-    }
+    return true
+  }
+  logout(): Observable<any> {
+    return this.http.get('http://localhost:8087/user/logout', { responseType: 'text' })
+  }
 }
